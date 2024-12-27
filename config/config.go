6ebd6585e8d231cf730a2831bc7a3acc6be1c9be
Naufalnurfahriza <123190024@student.upnyk.ac.id>
2024-12-27 16:21:05 +0700

    update config.go

--- config/config.go
+++ config/config.go
@@ -14,7 +14,7 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	err := godotenv.Load()
+	err := godotenv.Load("config/.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
@@ -26,6 +26,11 @@ func InitDB() {
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
 	)
+	fmt.Println("DB_HOST:", os.Getenv("DB_HOST"))
+	fmt.Println("DB_PORT:", os.Getenv("DB_PORT"))
+	fmt.Println("DB_USER:", os.Getenv("DB_USER"))
+	fmt.Println("DB_PASSWORD:", os.Getenv("DB_PASSWORD"))
+	fmt.Println("DB_NAME:", os.Getenv("DB_NAME"))
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
