8383d92f52ff030440637b32052b4a8124ac5105
Naufalnurfahriza <123190024@student.upnyk.ac.id>
2024-12-30 10:36:26 +0700

    deploy railway

--- config/config.go
+++ config/config.go
@@ -20,11 +20,11 @@ func InitDB() *gorm.DB { // Mengembalikan *gorm.DB
 	}
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
+		os.Getenv("PGHOST"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+		os.Getenv("PGDATABASE"),
+		os.Getenv("PGPORT"),
 	)
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
